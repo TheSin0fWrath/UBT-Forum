109eac363b8f422145a18ea8ac018b27941d9517
TheSin0fWrath <63659503+TheSin0fWrath@users.noreply.github.com>
2021-07-14 20:42:56 +0200

    Revert "Merge branch 'Era' into Merge"
    This reverts commit 5941c20b6553659553e682f530b6eedf661af4eb, reversing
    changes made to 4874b78f9472de0df89c9a60cc0b6656ccb44be4.

--- frontend/src/UserProfile/UserReputation.js
+++ frontend/src/UserProfile/UserReputation.js
@@ -1,175 +1,36 @@
-import React, { useContext } from "react";
-import "./Reputation.css";
+import React,{useContext} from "react";
 import { UserContext } from '../Shared/hooks/UserContext';
-import EmptyPage from "../Shared/Components/EmptyPage";
-import RepPage from "../Shared/Components/RepPage";
-import { CheckLogin } from "../Shared/hooks/CheckLogin";
-import getUser from "./UerInfoCrud";
-const userid = window.location.pathname.split("/").pop();
-const fetchURL = 'http://localhost:5000/user/getuser/' + userid;
-const getRepListApi = () => fetch("http://localhost:5000/api/reputation/" + userid).then(response => response.json());
+import EmptyPage from "../Shared/Components/EmptyPage"
 
-export default function UserReputation() {
-    const [isBusy, setBusy] = React.useState(true);
-    const [repId, setRepId] = React.useState([]);
-    const [getRepId, setGetRepId] = React.useState([]);
-    const [getRepDetails, setGetRepDetails] = React.useState({ repRate: "2" });
-    const [getRepList, setGetRepList] = React.useState([]);
-    const { user, setUser } = useContext(UserContext);
-    const [currentUser, setCurrentUser] = React.useState([]);
+export default function UserReputation(){
+    const {user,setUser} = useContext(UserContext);
 
-
-    React.useEffect(async () => {
-
-        async function fetchMyUser() {
-
-            let response = await fetch(fetchURL);
-            response = await response.json();
-            setRepId(response.data);
-            setBusy(false);
-        }
-        fetchMyUser();
-
-        getRepListApi().then(res => setGetRepList(res));
-    }, []);
-    function addRep() {
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json', "Authorization": `Bearer ${window.localStorage.getItem("token")}` },
-            body: JSON.stringify({
-                Message: getRepDetails.repMessage,
-                Reputation: getRepDetails.repRate,
-                fromUserId: user.nameid,
-                ToUserId: userid
-            })
-        };
-        fetch("http://localhost:5000/api/reputation", requestOptions).then(response => response.json()).then();
-    }
-    function deleteRep(e) {
-        let id = e.target.id;
-        const requestOptions = {
-            method: 'DELETE',
-            headers: { 'Content-Type': 'application/json', "Authorization": `Bearer ${window.localStorage.getItem("token")}` },
-            body: JSON.stringify({})
-        };
-        fetch('http://localhost:5000/api/reputation/' + id, requestOptions);
-    }
-    function editRep(e) {
-        let id = e.target.id;
-        const requestOptions = {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json', "Authorization": `Bearer ${window.localStorage.getItem("token")}` },
-            body: JSON.stringify({
-                Message: getRepDetails.repMessage,
-                Reputation: getRepDetails.repRate,
-                fromUserId: user.nameid,
-                ToUserId: userid
-            })
-        };
-        fetch('http://localhost:5000/api/reputation/' + id, requestOptions);
-        // console.log(e.target.id);
-    }
-
-    const renderep = getRepList.map(rep => {
-        console.log(rep.fromuser)
-        console.log(user.nameid)
-
-        if (rep.points > 0) {
-
-            if (user.nameid == rep.fromuser) {
-
-                return (
-                    <li key={rep.id} className="replist">
-                        <li>{rep.username}
-                        </li><span className="posrep">Positive({rep.points}):   </span> {rep.message}
-                        <span id={rep.id} onClick={deleteRep}> Delete -</span>
-                        <span id={rep.id} onClick={editRep}> Edit </span>
-                    </li>)
-            }
-            else {
-                return (
-                    <li key={rep.id} className="replist">
-                        <li>{rep.username}
-                        </li><span className="posrep">Positive({rep.points}):   </span> {rep.message}
-                    </li>)
-            }
-        }
-
-        else {
-            if (user.nameid == rep.fromuser) {
-                return (
-                    <li key={rep.id} className="replist">
-                        <li>{rep.username}
-                        </li><span className="negrep">Positive({rep.points}):   </span> {rep.message}
-                        <span id={rep.id} onClick={deleteRep}>  Delete -</span><br />
-                        <span id={rep.id} onClick={editRep}> Edit </span>
-                    </li>
-
-                )
-
-            }
-            else {
-                return (
-                    <li key={rep.id} className="replist">
-                        <li>{rep.username}
-                        </li><span className="negrep">Positive({rep.points}):   </span> {rep.message}
-                    </li>
-                )
-            }
-        }
-
-    })
-
-
-
-    return (
+    return(
         <div className="Reputation">
-            <RepPage path="/id">
+        <EmptyPage path="/reputation">
+        <div className="ContentBox">
+               <div className="ContentBoxHeader">
+                  <h3>Reputation for Sead </h3>
+               </div>
+               
+            </div>
+            <div className="reputationstats">
+            <h1>Total Reputation</h1>
 
-                <div className="ContentBox">
-                    <div className="ContentBoxHeader">
-                        <h3>Reputation for {isBusy ? ('null') : (<p>{repId.username}</p>)} </h3>
-                    </div>
-                    <div className="submitRep">
-                        <select onChange={event => setGetRepDetails({
-                            ...getRepDetails,
-                            repRate: event.target.value
-                        })} name="submitUserRep" id="userRepSelect">
-                            <option value="2">Positive</option>
-                            <option value="0">Neutral</option>
-                            <option value="-2">Negative</option>
-                        </select>
-                        <input onChange={event => setGetRepDetails({
-                            ...getRepDetails,
-                            repMessage: event.target.value
-                        })} type="text" placeholder="Write a comment" />
-                        <button onClick={addRep}>Rate</button>
-                    </div>
-                </div>
-                <div className="reputationstats">
-                    <h1>Total Reputation</h1>
-
-                </div>
-                <div className="reputationComments">
-                    <ul>
-                        {/* {getRepList.map(repList => ( 
-                        
-                    <li key={repList.id}>
-                        {repList.fromUser} <span>
-                            {(repList.reputation > 0)? 'Positive(': ((repList.reputation == 0)? 'Neutral(': 'Negative(')}
-                            {repList.reputation}) </span> 
-                        <p>{repList.message}</p>
-                        
-                    </li> ))} */}
-                        {
-                            renderep
-                        }
-
-
-
-                    </ul>
-                </div>
-            </RepPage>
+            </div>
+            <div className="reputationComments">
+                <ul>
+                    <li className="replist">
+                        <li>seadi
+                        </li><span className="posrep">Positive(-1):   </span> kew
+                        </li>
+                     <li className="replist">
+                        <li>seadi
+                        </li><span className="negrep">Positive(-1):   </span> kew
+                        </li>
+                </ul>
+            </div>
+        </EmptyPage>
         </div>
 
 
